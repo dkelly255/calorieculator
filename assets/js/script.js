0592d10aa7ec4f1464ebd375ad80778580691046
dkelly255 <dkelly25@hotmail.co.uk>
2021-09-24 20:51:18 +0000

    Build JS logic for calorie calculation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,49 @@
-function calculateCalories () {
-    let age = document.getElementById("age").value;
-    let weight = document.getElementById("weight").value;
-    let height = document.getElementById("height").value;
-    let gender = document.getElementById("gender").value;
-    let activity = document.getElementById("activity").value
+// let calories
+let calculatePdct = document.getElementById("calculate-pdct");
+console.log(calculatePdct)
+calculatePdct.addEventListener("click", calculateCalories);
+console.log(calculatePdct)
+// document.getElementById("display-pdct").innerHTML = calories
 
-    if (gender = "Male") {
-        let bmr = 66 + (6.3 * weight) + (12.9 * height) - (6.8 * age); 
+function calculateCalories (event) {
+    event.preventDefault();
+    
+    let age = document.getElementById("age").value;
+    console.log(age)
+    let weight = document.getElementById("weight").value;
+    console.log(weight)
+    let height = document.getElementById("height").value;
+    console.log(height)
+    let gender = document.getElementById("gender").value;
+    console.log(gender)
+    let activity = document.getElementById("activity").value
+    console.log(activity)
+
+    if (gender = "male") {
+        bmr = 66 + (6.3 * weight) + (12.9 * height) - (6.8 * age); 
+        console.log(bmr)
     } 
     else {
-        let bmr = 655 + (4.3 * weight) + (4.7 * height) - (4.7 * age); 
+        bmr = 655 + (4.3 * weight) + (4.7 * height) - (4.7 * age); 
     }
-
-    if (activity = "Sedentary") {
-        let multiplier = 1.2
-    } else if (activity = "Lightly Active") {
-        let multiplier = 1.375
-    } else if (activity = "Moderately Active") {
-        let multiplier = 1.55 
-    } else if (activity = "Very Active") {
-        let multiplier = 1.725
+    console.log(bmr)
+    console.log(age)
+    if (activity = "sedentary") {
+        multiplier = 1.2
+    } else if (activity = "lightlyActive") {
+        multiplier = 1.375
+    } else if (activity = "moderatelyActive") {
+        multiplier = 1.55 
+    } else if (activity = "veryActive") {
+        multiplier = 1.725
     } else {
-        let multiplier = 1.9
+        multiplier = 1.9
     } 
-    
+    console.log(bmr)
+    console.log(multiplier)
     let calories = bmr * multiplier
-
+    console.log(calories)
+    return calories
+    
 }
+console.log(calories)
