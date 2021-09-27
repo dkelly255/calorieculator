8f8941baad75cc08f3e7ece6a466c8d57a2b854f
dkelly255 <dkelly25@hotmail.co.uk>
2021-09-27 12:31:02 +0000

    Develop function to record chosen macro split from macros.html DOM

--- assets/js/macros.js
+++ assets/js/macros.js
@@ -18,12 +18,30 @@ macroChoice.addEventListener("click", splitMacros);
     console.log(document.getElementsByName("macro_choice"));
 let macroArray = document.getElementsByName("macro_choice")
     console.log(macroArray);
-    console.log(macroArray[1]);
+    console.log(macroArray[1].checked);
     console.log(document.getElementsByName("macro_choice")[1]);
     console.log(document.getElementsByName("macro_choice")[1]);
-    
-    
+let chosenOption    
 
     function splitMacros (event) {
         event.preventDefault();
+        
+        if (macroArray[0].checked) {
+            console.log("StandardChecked")
+            chosenOption = "standard"
+            console.log(chosenOption)
+        } else if (macroArray[1].checked) {
+            console.log("HighProteinChecked")
+            chosenOption = "highprotein"
+            console.log(chosenOption)
+        } else if (macroArray[2].checked) {
+            console.log("LowCarbChecked")
+            chosenOption = "lowcarb"
+            console.log(chosenOption)
+        } else {
+            console.log("NothingChecked")
+            chosenOption = "notselected"
+            console.log(chosenOption)
+        }
+        
     }
