48415888d57d93f0373d09a91daee729cdb67c44
dkelly255 <dkelly25@hotmail.co.uk>
2021-09-27 13:10:51 +0000

    Add Macro Mix function to macros.js

--- assets/js/macros.js
+++ assets/js/macros.js
@@ -1,8 +1,8 @@
  
- console.log(localStorage.pdct)
- console.log(document.getElementById("carry-pdct"));
+    console.log(localStorage.pdct)
+    console.log(document.getElementById("carry-pdct"));
  
- let carryPdct = document.getElementById("carry-pdct");
+let carryPdct = document.getElementById("carry-pdct");
     console.log(carryPdct)
 carryPdct.addEventListener("click", importPdct);
     console.log(carryPdct)
@@ -21,27 +21,42 @@ let macroArray = document.getElementsByName("macro_choice")
     console.log(macroArray[1].checked);
     console.log(document.getElementsByName("macro_choice")[1]);
     console.log(document.getElementsByName("macro_choice")[1]);
+
 let chosenOption    
 
     function splitMacros (event) {
         event.preventDefault();
         
         if (macroArray[0].checked) {
-            console.log("StandardChecked")
+                console.log("StandardChecked")
             chosenOption = "standard"
-            console.log(chosenOption)
+                console.log(chosenOption)
         } else if (macroArray[1].checked) {
-            console.log("HighProteinChecked")
+                console.log("HighProteinChecked")
             chosenOption = "highprotein"
-            console.log(chosenOption)
+                console.log(chosenOption)
         } else if (macroArray[2].checked) {
-            console.log("LowCarbChecked")
+                console.log("LowCarbChecked")
             chosenOption = "lowcarb"
-            console.log(chosenOption)
+                console.log(chosenOption)
         } else {
-            console.log("NothingChecked")
+                console.log("NothingChecked")
             chosenOption = "notselected"
-            console.log(chosenOption)
+                console.log(chosenOption)
         }
-        
+       
+        console.log(chosenOption)
     }
+    console.log(chosenOption)
+    
+let splitButton = document.getElementById("split-macros");
+    console.log(splitButton)
+splitButton.addEventListener("click", mixMyMacros);
+    console.log(splitButton)
+
+function mixMyMacros (event) {
+    document.getElementById("protein").innerHTML=localStorage.pdct * 0.25
+    document.getElementById("carbs").innerHTML=localStorage.pdct * .35
+    document.getElementById("fat").innerHTML=localStorage.pdct * .4
+    document.getElementById("total").innerHTML=localStorage.pdct
+}
