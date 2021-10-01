8ed8832c2d76d0d8734f01f66953db63f38d530d
dkelly255 <dkelly25@hotmail.co.uk>
2021-10-01 11:27:08 +0000

    Continue Meal generator function development

--- assets/js/meals.js
+++ assets/js/meals.js
@@ -34,30 +34,92 @@ document.getElementById('lunch-fat-calories').innerHTML=(Math.round(lunchTotalCa
 
 
 
+// Fixed lookup index of foods per macronutrient - food name and calories per gram for meal generator grams column
+const proteinIndex = [
+    {
+      "name": "chicken",
+      "calpergram": 2,      
+    },
+    {
+        "name": "Turkey",
+        "calpergram": 7,
+    },
+    {
+        "name": "Fish",
+        "calpergram": 15,
+    },
+    {
+        "name": "beef",
+        "calpergram": 9,
+    },
+    {
+        "name": "eggs",
+        "calpergram": 12.2,
+    },
+    {
+        "name": "pork",
+        "calpergram": 11,
+    },
+  ]
+    console.log(proteinIndex)
+let proteinIndexLength = proteinIndex.length
+    console.log(proteinIndexLength)
 
+// proteinFoods =  list of all possible foods that can be selected by user - baseline options for random meal generator
 const proteinFoods = document.getElementsByClassName("protein-input")
 let numberOfFoods = proteinFoods.length;
-console.log(numberOfFoods)
+    console.log(numberOfFoods)
 
+// proteinArray = foods selected by user for inclusion in meal generator options - extracted from proteinFoods
 let proteinArray = []
-console.log(proteinArray)
+    console.log(proteinArray)
 
+// function to extract which foods are checked by user, and push onto proteinArray 
 for (let i = 0; i < numberOfFoods; i++) {
     if (proteinFoods[i].checked) {
     proteinArray.push(proteinFoods[i].name);
     }
 }
 
-console.log(proteinArray)
-console.log(proteinArray[0])
-console.log(proteinFoods[0].checked)
-console.log(proteinArray.length)
-console.log(proteinFoods.length)
+    console.log(proteinArray)
+    console.log(proteinArray[0])
+    console.log(proteinFoods[0].checked)
+    console.log(proteinArray.length)
+    console.log(proteinFoods.length)
 
+// Random number between 1 and how many food have been checked per food group - used to pick a random checked food's name for meal plan buildout
 const rand1 = Math.floor( Math.random() * (proteinArray.length - 1) );
     console.log(rand1);
+const food1 = proteinArray[rand1]
+document.getElementById("breakfast-protein-food").innerHTML=(food1)
+    console.log(rand1);
+    console.log(proteinIndex[rand1].name)
+    console.log(proteinIndex[rand1].calpergram)
+    console.log(proteinArray[rand1])
+    
 
-    document.getElementById("breakfast-protein-food").innerHTML=(proteinArray[rand1])
+  
+    console.log(proteinIndex.find(x => x.name === 'chicken').calpergram)
+    console.log('hello')
+
+    // for (let i = 0; i < proteinIndexLength; i++) {
+    //     if (proteinIndex[i].name = proteinArray[rand1]) {
+            
+    //         // document.getElementById("breakfast-protein-grams").innerHTML=(proteinIndex[i].calpergram);
+    //         console.log(proteinIndex[i].name)
+    //         console.log(proteinArray[rand1])
+    //         console.log[i]
+    //     } else {
+    //         console.log("food not in array")
+    //     }
+    // }
+    console.log(rand1);
+    
+
+
+
+// document.getElementById("breakfast-protein-grams").innerHTML=(proteinArray[rand1])
+//     console.log(rand1);
 
 }
 
