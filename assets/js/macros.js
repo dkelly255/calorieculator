e7ee882d796e3fa491716fe6d22794ba90de0852
dkelly255 <dkelly25@hotmail.co.uk>
2021-10-07 09:17:33 +0000

    Add code comments to macros.js

--- assets/js/macros.js
+++ assets/js/macros.js
@@ -1,28 +1,36 @@
 /* jshint esversion: 8 */
 
-console.log(localStorage.pdct);
-console.log(document.getElementById("carry-pdct"));
-
+    console.log(localStorage.pdct);
+    console.log(document.getElementById("carry-pdct"));
+// Set a variable equal to the DOM element button with id "carry-pdct" 
 let carryPdct = document.getElementById("carry-pdct");
-console.log(carryPdct);
+    console.log(carryPdct);
+// Attach an event listener to the variable, to run function "importPdct" when a "click" event occurs
 carryPdct.addEventListener("click", importPdct);
-console.log(carryPdct);
-
+    console.log(carryPdct);
+// Defining the importPdct function to be ran by event listener above
 function importPdct() {
+    /*Take the pdct from local storage and set it to the inner HTML contents of the DOM element "pdct-box"
+    This displays/imports the user's pdct from the previous page in preparation for the application of 
+    Macro-nutrient split option selections */
     document.getElementById("pdct-box").innerHTML = localStorage.pdct;
 }
-
-
+/* Set a variable equal to the DOM element button with id "calculate-macros" - this button will be hidden
+ using further instructions below, until the user has selected a split option */
 let macroChoice = document.getElementById("calculate-macros");
+// Add the "hide" class to the button above
 macroChoice.classList.add("hide");
-console.log(macroChoice);
+    console.log(macroChoice);
+// Add event listener to the button, to run function "splitMacros" when a "click" event occurs 
 macroChoice.addEventListener("click", splitMacros);
-console.log(macroChoice);
-console.log(document.getElementsByName("macro_choice"));
+    console.log(macroChoice);
+    console.log(document.getElementsByName("macro_choice"));
+/* Create an array of all DOM elements with name "macro_choice" (i.e all radio buttons) and store
+in variable macroArray*/
 let macroArray = document.getElementsByName("macro_choice");
-console.log(macroArray);
-console.log(macroArray[1].checked);
-console.log(document.getElementsByName("macro_choice")[1]);
+    console.log(macroArray);
+    console.log(macroArray[1].checked);
+    console.log(document.getElementsByName("macro_choice")[1]);
 
 // find all radio buttons
 const radios = document.querySelectorAll("input[name='macro_choice']");
@@ -38,12 +46,14 @@ function checkRadio() {
         macroChoice.classList.remove("hide");
     }
 }
-
+// Declare variable for use in function splitMacros to check which macro option the user has selected
 let chosenOption;
-
+/* Function used by event listener on line 25 to go through each macroArray element and check if it has been selected, applying the 
+appropriate macronutrient split in each case*/
 function splitMacros(event) {
+    // Prevent default submission of form when button is clicked
     event.preventDefault();
-
+    // Apply the "standard" macronutrient split if "standard" radio button has been selected by user 
     if (macroArray[0].checked) {
 
         chosenOption = "standard";
@@ -54,7 +64,7 @@ function splitMacros(event) {
         document.getElementById("total").innerHTML = localStorage.pdct;
         localStorage.setItem("macro", "standard");
         console.log(localStorage);
-
+    // Apply the "high protein" macronutrient split if the "high protein" radio button has been selected by user 
     } else if (macroArray[1].checked) {
 
         chosenOption = "highprotein";
@@ -65,7 +75,7 @@ function splitMacros(event) {
         document.getElementById("total").innerHTML = localStorage.pdct;
         localStorage.setItem("macro", "highprotein");
         console.log(localStorage);
-
+    // Apply the "lowcarb" macronutrient split if the "low carb" radio button has been selected by user 
     } else if (macroArray[2].checked) {
 
         chosenOption = "lowcarb";
